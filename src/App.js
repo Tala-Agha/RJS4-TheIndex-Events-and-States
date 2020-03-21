e0d01ed34a9821ddf6da39a227fc34fa07609cc6
Tala Agha <macbook@MacBooks-MacBook-Pro.local>
2020-03-21 15:32:53 +0200

    React T4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 // Data
 import authors from "./data";
@@ -6,20 +6,41 @@ import authors from "./data";
 // Components
 import Sidebar from "./Sidebar";
 import AuthorList from "./AuthorList";
+import AuthorDetail from "./AuthorDetail";
 
-function App() {
-  return (
-    <div id="app" className="container-fluid">
-      <div className="row">
-        <div className="col-2">
-          <Sidebar />
-        </div>
-        <div className="content col-10">
-          <AuthorList authors={authors} />
+class App extends Component {
+  state = {
+    currentAuthor: null
+  };
+
+  selectAuthor = author => this.setState({ currentAuthor: author });
+  deselectAuthor = () => this.setState({ currentAuthor: null });
+
+  getContentView = () => {
+    if (this.state.currentAuthor) {
+      return <AuthorDetail author={this.state.currentAuthor} />;
+    } else {
+      return (
+        <AuthorList
+          authors={authors}
+          selectAuthor={this.selectAuthor}
+          filterAuthors={this.filterAuthors}
+        />
+      );
+    }
+  };
+  render() {
+    return (
+      <div id="app" className="container-fluid">
+        <div className="row">
+          <div className="col-2">
+            <Sidebar deselectAuthor={this.deselectAuthor} />
+          </div>
+          <div className="content col-10">{this.getContentView()}</div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
